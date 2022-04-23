48214bd0017aa5847ac35572ca9bd2abcec8cef5
mohamed-jadeer <mohjadeer@gmail.com>
2022-04-23 16:15:26 +0530

    Pyramid Pattern

---
+++ src/fundamentals/PyramidPattern.java
@@ -0,0 +1,20 @@
+package day1;
+import java.util.Scanner;
+public class PyramidPattern {
+	public static void main(String[] args){
+		System.out.println("Height of the pyramid required : ");
+		Scanner sc = new Scanner(System.in);
+		int height = sc.nextInt();
+		sc.close();
+		for(int i=0 ; i <  height ; i++){
+			for(int j = 0 ; j < ((height*2)-1) ; j++){
+				if((j<((height-1)-i)) || ((j>(height-1)+i))){
+					System.out.print(" ");
+				}
+				else
+					System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
