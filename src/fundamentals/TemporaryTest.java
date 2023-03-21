950680a3a505af61b08697db16d92cf02e17be71
Jadeer <mohjadeer@gmail.com>
2023-03-21 22:53:11 +0530

    TempTest

---
+++ src/fundamentals/TemporaryTest.java
@@ -0,0 +1,18 @@
+package fundamentals;
+
+//import static java.lang.Math;
+
+public class TemporaryTest {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+        byte num = 3;
+
+        Organism org = new Organism();
+        Fish fish = (Fish) null;
+
+
+    }
+
+}
